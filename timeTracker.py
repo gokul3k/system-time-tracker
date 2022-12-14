368d644e8dfd9e53a5b4d2a2bf4cfba42ee0f28a
Gokul Kamalakannan (Nokia) <gokul.kamalakannan@nokia.com>
2022-12-13 21:01:25 -0500

    first commit

---
+++ timeTracker.py
@@ -0,0 +1,20 @@
+from win32gui import GetForegroundWindow
+import psutil
+import time
+import win32process
+
+process_time={}
+timestamp = {}
+
+
+while True:
+    current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
+    timestamp[current_app] = int(time.time())
+    time.sleep(1)
+    print(process_time.keys())
+    if current_app not in process_time.keys():
+        process_time[current_app] = 0
+
+    process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
+    print(process_time)
+    print(timestamp)
