124c007b4fe2999dcb1163d7b3f6d2f786ba6af1
Gokul Kamalakannan (Nokia) <gokul.kamalakannan@nokia.com>
2022-12-14 02:02:47 -0500

    Saves all data to a single dictionary and export it as json on script termination

---
+++ test1.py
@@ -0,0 +1,65 @@
+# from AppKit import NSWorkspace
+# import time
+#
+# activeWindowTime = ""
+#
+# while True:
+#     new_window_name = (NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName'])
+#
+#     if active_window_lane != new_window_name:
+#         active_window_name = new_window_name
+#         print(active_window_name)
+#
+#     time.sleep(10)
+
+# test = {}
+# current_app = 'Gopal'
+#
+# if current_app not in test.keys():
+#     test.update({current_app: '45'})
+#
+# print('Done...')
+
+# import csv
+# import atexit
+# import ast
+#
+# csv_columns = ['AppName', 'Time Spend']
+# csv_file = "log.csv"
+# all_apps = {'pycharm64': '66', 'vpnui': '15', 'OUTLOOK.EXE': '6', 'Teams': '1', 'msedge': '1'}
+#
+#
+#
+# def exit_handler(all_apps):
+#     with open(csv_file,  encoding="utf-8-sig") as csvfile:
+#         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+#         writer.writeheader()
+#         writer.writerow(all_apps)
+#
+#
+# atexit.register(exit_handler(all_apps))
+
+
+# import json
+#
+# # Data to be written
+# dictionary = {
+# 'pycharm64': 66,
+#     'vpnui': 1,
+#     'OUTLOOK.EXE': 6,
+#     'Teams': 1,
+#     'msedge': 25
+# }
+#
+# # Serializing json
+# with open("sample.json", "w") as outfile:
+#     json.dump(dictionary, outfile)
+import os
+import getpass
+path = os.path.join('..','Documents and Settings',os.getlogin(),'Desktop')
+path2 = os.path.join('..','Documents and Settings',getpass.getuser(),'Desktop')
+path3 = os.getlogin()
+print(path)
+print(path2)
+print(path3)
+
--- timeTracker.py
+++ timeTracker.py
@@ -2,19 +2,46 @@ from win32gui import GetForegroundWindow
 import psutil
 import time
 import win32process
+import json
+import atexit
+import os
 
-process_time={}
+path = os.getlogin()
+process_time = {}
 timestamp = {}
-
+all_apps = {'UserName': path}
 
 while True:
-    current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
-    timestamp[current_app] = int(time.time())
-    time.sleep(1)
-    print(process_time.keys())
-    if current_app not in process_time.keys():
-        process_time[current_app] = 0
+    try:
+        current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(
+            ".exe", "")
+        pid = win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]
+        if pid < 0:
+            pass
+    except psutil.NoSuchProcess:
+        pass
+    else:
+        timestamp[current_app] = int(time.time())
+        time.sleep(1)
+        if current_app not in process_time.keys():
+            process_time[current_app] = 0
 
-    process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
-    print(process_time)
-    print(timestamp)
+        process_time[current_app] = process_time[current_app] + int(time.time()) - timestamp[current_app]
+
+        if current_app not in all_apps.keys():
+            key_value = process_time[current_app]
+            all_apps.update({current_app: key_value})
+        else:
+            new_key_value = all_apps[current_app] + process_time[current_app]
+            all_apps.update({current_app: new_key_value})
+
+        # print(process_time)
+        # print(timestamp)
+        # print(all_apps)
+
+
+        def exit_handler():
+            with open("log.json", "w") as outfile:
+                json.dump(all_apps, outfile)
+
+        atexit.register(exit_handler)
